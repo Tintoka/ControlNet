cd78eb7f2b97597894f6f5656b4ce359af320503
Tintoka <Tintoka@aut.ac.ir>
2025-12-24 00:36:28 +0330

    add apply_canny in ControlNet/canny

--- annotator/canny/__init__.py
+++ annotator/canny/__init__.py
@@ -1,6 +1,9 @@
 import cv2
 
-
 class CannyDetector:
     def __call__(self, img, low_threshold, high_threshold):
         return cv2.Canny(img, low_threshold, high_threshold)
+
+def apply_canny(img, low_threshold, high_threshold):
+    # This wrapper function makes the class compatible with your model.py
+    return CannyDetector()(img, low_threshold, high_threshold)
